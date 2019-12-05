754c49985497b8b87c7c9bf890824fd2f542e2a2
1293169930 <1293169930@qq.com>
2019-12-05 14:48:59 +0800

    第二次提交

--- src/org/java/demo/Second.java
+++ src/org/java/demo/Second.java
@@ -1,10 +1,7 @@
 package org.java.demo;
 
-public class Demo {
+public class Second {
     public static void main(String[] args) {
-        System.out.println("11111111111111111111");
+        System.out.println("222222222222222");
     }
-
-
-
 }
